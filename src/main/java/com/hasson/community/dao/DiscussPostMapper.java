8621cdb2248ad3d5efb8827b9dae719856743de6
hasson <hassonl@foxmail.com>
2021-01-20 14:27:40 +0800

    查找帖子

---
+++ src/main/java/com/hasson/community/dao/DiscussPostMapper.java
@@ -0,0 +1,17 @@
+package com.hasson.community.dao;
+
+import com.hasson.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+    //offset 开始的id,limit 显示多少个（每个多少个）
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    //一定要@Param（用于给参数取别名）注解，
+    //如果只有一个参数，并且在<if>里使用，则必须加别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+}
--- src/test/java/com/hasson/community/MapperTest.java
+++ src/test/java/com/hasson/community/MapperTest.java
@@ -1,6 +1,8 @@
 package com.hasson.community;
 
+import com.hasson.community.dao.DiscussPostMapper;
 import com.hasson.community.dao.UserMapper;
+import com.hasson.community.entity.DiscussPost;
 import com.hasson.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,16 +11,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
 public class MapperTest {
     @Autowired
-    private UserMapper userMapper;
+    private DiscussPostMapper discussPostMapper;
 
     @Test
     public void testSelectUser() {
-        User user = userMapper.selectById(103);
-        System.out.println(user);
+//        List<DiscussPost> discussPosts = discussPostMapper.selectDiscussPosts(0, 5, 10);
+//        for (DiscussPost d :
+//                discussPosts) {
+//            System.out.println(discussPosts);
+//        }
+        System.out.println(discussPostMapper.selectDiscussPostRows(0));
     }
 }
